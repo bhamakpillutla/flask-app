db9d40472bb4c543a3c97f571796688100c1d3bf
Bhama Pillutla <65846493+bhamakpillutla@users.noreply.github.com>
2023-11-29 17:27:49 -0800

    Update server.py

--- server.py
+++ server.py
@@ -6,18 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    response = {
-            'message': 'Hello, world!',
-            'status': 'success'
-            }
-    # time.sleep(2) # Simulate a 2-second delay
-    app.logger.warning('A warning occurred')
-    app.logger.error('An error occurred')
-    app.logger.info('Info')
-    # Intentionally generate bad responses
-    response['message']= 'Internal Server Error'
-    response['status'] = 'error'
-    return jsonify(response), 500
-
+    return 'hello'
 if __name__=='__main__':
     app.run(debug=False,host='0.0.0.0', port = 7000)
